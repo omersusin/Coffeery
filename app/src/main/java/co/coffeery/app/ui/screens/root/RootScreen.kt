abe30d6de550139121ae84e2718f6ba9b79d8334
omersusin <tekgercek73@gmail.com>
2026-07-11 22:01:39 +0000

    fix: add missing RootScreen closing brace + padding/dp imports

--- app/src/main/java/co/coffeery/app/ui/screens/root/RootScreen.kt
+++ app/src/main/java/co/coffeery/app/ui/screens/root/RootScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,6 +24,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import co.coffeery.app.R
 import co.coffeery.app.ui.components.BottomNav
@@ -96,6 +98,7 @@ fun RootScreen(vm: AppViewModel) {
                 }
             }
         }
+    }
     }
 }
 
